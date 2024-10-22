e61c4b91b0aa503b07d50a7159cb4d0320f8ac75
CristianCamiloRT <cristianramirez.desarrollador@hotmail.com>
2024-10-21 23:36:44 -0500

    Add initial migrations

---
+++ database/migrations/2024_10_22_014249_create_visits_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('entry_date');
+            $table->dateTime('exit_date')->nullable();
+            $table->integer('interior');
+            $table->integer('house');
+            $table->string('name');
+            $table->string('document');
+            $table->string('authorized_by');
+            $table->text('observations')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('visits');
+    }
+};
---
+++ database/migrations/2024_10_22_015832_create_vehicles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('entry_date');
+            $table->dateTime('exit_date')->nullable();
+            $table->integer('interior');
+            $table->integer('house');
+            $table->string('plate');
+            $table->string('brand');
+            $table->string('left_state', 10);
+            $table->string('right_state', 10);
+            $table->string('back_state', 10);
+            $table->string('front_state', 10);
+            $table->text('observations')->nullable();
+            $table->string('antenna', 2);
+            $table->string('frontal', 2);
+            $table->string('spare_parts', 2);
+            $table->string('mirrors', 2);
+            $table->string('lights', 2);
+            $table->string('stops', 2);
+            $table->string('glasses', 2);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
