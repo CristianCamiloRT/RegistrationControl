d7ce293b100fc1a9fc2760f65237678fb5cb99b6
CristianCamiloRT <cristianramirez.desarrollador@hotmail.com>
2024-10-23 14:54:54 -0500

    Add user seed and role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(RolesSeed::class);
+        $this->call([
+            RolesSeed::class,
+            UserSeed::class,
+        ]);
+
     }
 }
---
+++ database/seeders/UserSeed.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeed extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('users')->insert(
+            ['name' => 'SuperAdmin', 'email' => 'admin@localhost.com', 'password' => Hash::make('ASDqwe123456'), 'role_id' => 1],
+            ['name' => 'Administración', 'email' => 'administracion@localhost.com', 'password' => Hash::make('administracion12345'), 'role_id' => 2],
+            ['name' => 'Guarda', 'email' => 'guarda1@localhost.com', 'password' => Hash::make('guarda12345'), 'role_id' => 3],
+        );
+    }
+}
