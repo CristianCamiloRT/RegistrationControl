81823da7930a35e4d540fbb10fc8971d7bd2f2e6
CristianCamiloRT <cristianramirez.desarrollador@hotmail.com>
2024-10-23 14:37:09 -0500

    New dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,26 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <div class="col-12">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+        <div class="card card-md">
+            <div class="card-stamp card-stamp-lg">
+                <div class="card-stamp-icon bg-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M5 11a7 7 0 0 1 14 0v7a1.78 1.78 0 0 1 -3.1 1.4a1.65 1.65 0 0 0 -2.6 0a1.65 1.65 0 0 1 -2.6 0a1.65 1.65 0 0 0 -2.6 0a1.78 1.78 0 0 1 -3.1 -1.4v-7"></path>
+                        <path d="M10 10l.01 0"></path>
+                        <path d="M14 10l.01 0"></path>
+                        <path d="M10 14a3.5 3.5 0 0 0 4 0"></path>
+                    </svg>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="row align-items-center">
+                    <div class="col-10">
+                        <h3 class="h1">Bienvenid@</h3>
+                        <div class="markdown text-secondary">
+                            {{ __("You're logged in!") }}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
