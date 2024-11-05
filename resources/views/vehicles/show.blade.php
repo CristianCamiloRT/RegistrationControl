1e1e51e8927dff5f92f29574ffb334453038aea7
CristianCamiloRT <cristianramirez.desarrollador@hotmail.com>
2024-11-05 16:33:59 -0500

    Add exit date and minutes fields to vehicle details view

--- resources/views/vehicles/show.blade.php
+++ resources/views/vehicles/show.blade.php
@@ -28,34 +28,48 @@
                 <div class="col-md-3">
                     <div class="mb-3">
                         <x-label-form class="form-label">Fecha-Hora Entrada</x-label-form>
-                        <x-input-form type="datetime-local" class="form-control"  :isInvalid="$errors->has('entry_date')" :value="$vehicle->entry_date" readonly/>
+                        <x-input-form type="datetime-local" class="form-control" :value="$vehicle->entry_date" readonly/>
                     </div>
                 </div>
+                @if($vehicle->exit_date)
+                    <div class="col-md-3">
+                        <div class="mb-3">
+                            <x-label-form class="form-label">Fecha-Hora Salida</x-label-form>
+                            <x-input-form type="datetime-local" class="form-control" :value="$vehicle->exit_date" readonly/>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="mb-3">
+                            <x-label-form class="form-label">Minutos en conjunto</x-label-form>
+                            <x-input-form type="number" class="form-control"  :value="$vehicle->minutes" readonly/>
+                        </div>
+                    </div>
+                @endif
                 <div class="card-header">
                     <h3 class="card-title" style="color: #307fdd;">Información</h3>
                 </div>
                 <div class="col-md-3">
                     <div class="mb-3">
                         <x-label-form class="form-label">Interior</x-label-form>
-                        <x-input-form type="number" class="form-control"  :isInvalid="$errors->has('interior')" :value="$vehicle->interior" readonly/>
+                        <x-input-form type="number" class="form-control" :value="$vehicle->interior" readonly/>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="mb-3">
                         <x-label-form class="form-label">Apto - Casa</x-label-form>
-                        <x-input-form type="number" class="form-control"  :isInvalid="$errors->has('house')" :value="$vehicle->house" readonly/>
+                        <x-input-form type="number" class="form-control" :value="$vehicle->house" readonly/>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="mb-3">
                         <x-label-form class="form-label">Placa</x-label-form>
-                        <x-input-form type="text" class="form-control"  :isInvalid="$errors->has('plate')" :value="$vehicle->plate" maxlength="8" style="text-transform: uppercase;" readonly/>
+                        <x-input-form type="text" class="form-control" :value="$vehicle->plate" maxlength="8" style="text-transform: uppercase;" readonly/>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="mb-3">
                         <x-label-form class="form-label">Marca</x-label-form>
-                        <x-input-form type="text" class="form-control"  :isInvalid="$errors->has('brand')" :value="$vehicle->brand" readonly/>
+                        <x-input-form type="text" class="form-control" :value="$vehicle->brand" readonly/>
                     </div>
                 </div>
                 <div class="card-header">
@@ -64,25 +78,25 @@
                 <div class="col-md-3">
                     <div class="mb-3">
                         <x-label-form class="form-label">Lateral Izq.</x-label-form>
-                        <x-select-input class="form-select"  :isInvalid="$errors->has('left_state')" :selected="$vehicle->left_state" :options="collect(['Bueno' => 'Bueno', 'Regular' => 'Regular', 'Malo' => 'Malo'])->prepend('Seleccione un opción', '')" disabled/>
+                        <x-select-input class="form-select" :selected="$vehicle->left_state" :options="collect(['Bueno' => 'Bueno', 'Regular' => 'Regular', 'Malo' => 'Malo'])->prepend('Seleccione un opción', '')" disabled/>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="mb-3">
                         <x-label-form class="form-label">Lateral Der.</x-label-form>
-                        <x-select-input class="form-select"  :isInvalid="$errors->has('right_state')" :selected="$vehicle->right_state" :options="collect(['Bueno' => 'Bueno', 'Regular' => 'Regular', 'Malo' => 'Malo'])->prepend('Seleccione un opción', '')" disabled/>
+                        <x-select-input class="form-select" :selected="$vehicle->right_state" :options="collect(['Bueno' => 'Bueno', 'Regular' => 'Regular', 'Malo' => 'Malo'])->prepend('Seleccione un opción', '')" disabled/>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="mb-3">
                         <x-label-form class="form-label">Anterior</x-label-form>
-                        <x-select-input class="form-select"  :isInvalid="$errors->has('back_state')" :selected="$vehicle->back_state" :options="collect(['Bueno' => 'Bueno', 'Regular' => 'Regular', 'Malo' => 'Malo'])->prepend('Seleccione un opción', '')" disabled/>
+                        <x-select-input class="form-select" :selected="$vehicle->back_state" :options="collect(['Bueno' => 'Bueno', 'Regular' => 'Regular', 'Malo' => 'Malo'])->prepend('Seleccione un opción', '')" disabled/>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="mb-3">
                         <x-label-form class="form-label">Posterior</x-label-form>
-                        <x-select-input class="form-select"  :isInvalid="$errors->has('front_state')" :selected="$vehicle->front_state" :options="collect(['Bueno' => 'Bueno', 'Regular' => 'Regular', 'Malo' => 'Malo'])->prepend('Seleccione un opción', '')" disabled/>
+                        <x-select-input class="form-select" :selected="$vehicle->front_state" :options="collect(['Bueno' => 'Bueno', 'Regular' => 'Regular', 'Malo' => 'Malo'])->prepend('Seleccione un opción', '')" disabled/>
                     </div>
                 </div>
                 <div class="card-header">
@@ -99,43 +113,43 @@
                 <div class="col-md-2">
                     <div class="mb-3">
                         <x-label-form class="form-label">Radio (Antena)</x-label-form>
-                        <x-select-input class="form-select"  :isInvalid="$errors->has('antenna')" :selected="$vehicle->antenna" :options="collect(['1' => 'Si', '0' => 'No'])->prepend('Seleccione un opción', '')" disabled/>
+                        <x-select-input class="form-select" :selected="$vehicle->antenna" :options="collect(['1' => 'Si', '0' => 'No'])->prepend('Seleccione un opción', '')" disabled/>
                     </div>
                 </div>
                 <div class="col-md-2">
                     <div class="mb-3">
                         <x-label-form class="form-label">Radio (Frontal)</x-label-form>
-                        <x-select-input class="form-select"  :isInvalid="$errors->has('frontal')" :selected="$vehicle->frontal" :options="collect(['1' => 'Si', '0' => 'No'])->prepend('Seleccione un opción', '')" disabled/>
+                        <x-select-input class="form-select" :selected="$vehicle->frontal" :options="collect(['1' => 'Si', '0' => 'No'])->prepend('Seleccione un opción', '')" disabled/>
                     </div>
                 </div>
                 <div class="col-md-2">
                     <div class="mb-3">
                         <x-label-form class="form-label">Repuesto</x-label-form>
-                        <x-select-input class="form-select"  :isInvalid="$errors->has('spare_parts')" :selected="$vehicle->spare_parts" :options="collect(['1' => 'Si', '0' => 'No'])->prepend('Seleccione un opción', '')" disabled/>
+                        <x-select-input class="form-select" :selected="$vehicle->spare_parts" :options="collect(['1' => 'Si', '0' => 'No'])->prepend('Seleccione un opción', '')" disabled/>
                     </div>
                 </div>
                 <div class="col-md-2">
                     <div class="mb-3">
                         <x-label-form class="form-label">Espejos</x-label-form>
-                        <x-select-input class="form-select"  :isInvalid="$errors->has('mirrors')" :selected="$vehicle->mirrors" :options="collect(['1' => 'Si', '0' => 'No'])->prepend('Seleccione un opción', '')" disabled/>
+                        <x-select-input class="form-select" :selected="$vehicle->mirrors" :options="collect(['1' => 'Si', '0' => 'No'])->prepend('Seleccione un opción', '')" disabled/>
                     </div>
                 </div>
                 <div class="col-md-2">
                     <div class="mb-3">
                         <x-label-form class="form-label">Farolas</x-label-form>
-                        <x-select-input class="form-select"  :isInvalid="$errors->has('lights')" :selected="$vehicle->lights" :options="collect(['1' => 'Si', '0' => 'No'])->prepend('Seleccione un opción', '')" disabled/>
+                        <x-select-input class="form-select" :selected="$vehicle->lights" :options="collect(['1' => 'Si', '0' => 'No'])->prepend('Seleccione un opción', '')" disabled/>
                     </div>
                 </div>
                 <div class="col-md-2">
                     <div class="mb-3">
                         <x-label-form class="form-label">Stops</x-label-form>
-                        <x-select-input class="form-select"  :isInvalid="$errors->has('stops')" :selected="$vehicle->stops" :options="collect(['1' => 'Si', '0' => 'No'])->prepend('Seleccione un opción', '')" disabled/>
+                        <x-select-input class="form-select" :selected="$vehicle->stops" :options="collect(['1' => 'Si', '0' => 'No'])->prepend('Seleccione un opción', '')" disabled/>
                     </div>
                 </div>
                 <div class="col-md-2">
                     <div class="mb-3">
                         <x-label-form class="form-label">Vidrios</x-label-form>
-                        <x-select-input class="form-select"  :isInvalid="$errors->has('glasses')" :selected="$vehicle->glasses" :options="collect(['1' => 'Si', '0' => 'No'])->prepend('Seleccione un opción', '')" disabled/>
+                        <x-select-input class="form-select" :selected="$vehicle->glasses" :options="collect(['1' => 'Si', '0' => 'No'])->prepend('Seleccione un opción', '')" disabled/>
                     </div>
                 </div>
             </div>
